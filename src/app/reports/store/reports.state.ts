e52e551bb98e563214bbb4d48c7476e7e3e878dd
Hirusha Fernando <heshanhfernando@gmail.com>
2023-02-02 23:02:17 +0530

    NgRx Changes In Reports

--- src/app/reports/store/reports.state.ts
+++ src/app/reports/store/reports.state.ts
@@ -1,15 +1,15 @@
 import { ArrearsReport, CashCollectionReport, CustomerReport, EPReport } from "../../types";
 
 export interface ReportsState {
-    epReport: EPReport[];
-    cashCollectionReport: CashCollectionReport[];
-    customerReport: CustomerReport[];
-    arrearsReport: ArrearsReport[];
+    epReport?: EPReport[];
+    cashCollectionReport?: CashCollectionReport[];
+    customerReport?: CustomerReport[];
+    arrearsReport?: ArrearsReport[];
 }
 
 export const initialState: ReportsState = {
-    epReport: new Array<EPReport>(),
-    cashCollectionReport: new Array<CashCollectionReport>(),
-    customerReport: new Array<CustomerReport>(),
-    arrearsReport: new Array<ArrearsReport>()
+    epReport: undefined,
+    cashCollectionReport: undefined,
+    customerReport: undefined,
+    arrearsReport: undefined
 }
